df082fca5fffa1b8dc50b461c0315d197a337f7a
Fyr <fyr@tut.by>
2013-12-12 18:07:51 +0200

    Сделал водяные знаки

--- app/plugins/media/controllers/router_controller.php
+++ app/plugins/media/controllers/router_controller.php
@@ -22,6 +22,7 @@ class RouterController extends MediaAppController {
 		App::import('Vendor', 'Image', array('file' => '../plugins/media/vendors/image.class.php'));
 		$image = new Image();
 		$image->load($this->PHMedia->getFileName($type, $id, null, $aFName['orig_fname'].'.'.$aFName['orig_ext']));
+
 		if ($aSize) {
 			$image->resize($aSize['w'], $aSize['h']);
 		}
@@ -30,6 +31,7 @@ class RouterController extends MediaAppController {
 		$media = $this->Media->findById($id);
 		$article = $this->Article->findById($media['Media']['object_id']);
 		if ($article['Article']['object_type'] == 'products') {
+			/*
 			$fontSize = 11;
 			$padY = 5; $padX = 10;
 			$fontFamily = '../plugins/captcha/webroot/fonts/tahoma.ttf';
@@ -37,7 +39,34 @@ class RouterController extends MediaAppController {
 			$textLen = $coord[2];
 			$image->filledRectangle($image->getSizeX() - $textLen - 2*$padX, 0, $image->getSizeX(), $fontSize + 2*$padY, array('CCCCCC', 'FFFFFF'));
 			$coord = imagettftext($image->getImage(), $fontSize, 0, $image->getSizeX() - $textLen - $padX, $fontSize + $padY, $image->getColor('0000FF'), $fontFamily, DOMAIN_TITLE);
+			*/
+
+			$logo = new Image();
+			$logo->load('./img/logo.gif');
+
+			// т.к. есть баг с ресайзом лого (при ресайзе исчезает прозрачность и появляется фон),
+			// то ресайзим саму картинку, а потом возвращаем ее в исх. размер или ресайзим в нужный
+			$oldSizeX = $image->getSizeX();
+			$oldSizeY = $image->getSizeY();
+			$lRestoreSize = false;
+			if ($aSize) {
+				if ($logo->getSizeX() > $image->getSizeX()) {
+					$image->resize($logo->getSizeX(), null);
+					$lRestoreSize = true;
+				}
+			} elseif ($image->getSizeX() > 400) {
+				$logo->load('./img/logo_big.gif');
+			}
+			$x = round(($image->getSizeX()) / 2, 0) - round($logo->getSizeX() / 2, 0);
+			$y = round(($image->getSizeY()) / 2, 0) - round($logo->getSizeY() / 2, 0);
+			imagealphablending($image->getImage(), false);
+			imagesavealpha($image->getImage(), true);
+			imagecopymerge($image->getImage(), $logo->getImage(), $x, $y, 0, 0, $logo->getSizeX(), $logo->getSizeY(), 40);
+			if ($lRestoreSize) {
+				$image->resize($oldSizeX, null);
+			}
 		}
+
 		if ($aFName['ext'] == 'jpg') {
 			$image->outputJpg($fname);
 			$image->outputJpg();
--- app/plugins/media/vendors/image.class.php
+++ app/plugins/media/vendors/image.class.php
@@ -2,55 +2,57 @@
 /**
 	Implements a basic functions for working with images
 **/
-class Image 
+class Image
 {
-	protected $rImage; // image resource to work with
-	
+	public $rImage; // image resource to work with
+
 	/**
 		Class constructor. Creates image resource and sets image size.
 		@param (int) $iSizeX - set image size X. Default value is 1.
 		@param (int) $iSizeY - set image size Y. Default value is 1.
 	**/
-	public function __construct($iSizeX = 1, $iSizeY = 1) 
+	public function __construct($iSizeX = 1, $iSizeY = 1)
 	{
 		$this->rImage = imagecreatetruecolor($iSizeX, $iSizeY);
+		// imagecolorallocatealpha($this->rImage, 255, 255, 255, 127);
 	}
 
 	/**
 		Loads image into inner buffer to work with. If image cannot be loaded, 'false' is returned.
 		@param (string) $_inFile - image file name with path
 	**/
-	public function load($_inFile) 
+	public function load($_inFile)
 	{
 		if ((list($src_w, $src_h, $src_type, $src_attr) = @getimagesize($_inFile)) === false)
 		{
 			return false;
 		}
-		
+
 		imagedestroy($this->rImage);
-		
-		switch ($src_type) 
+
+		switch ($src_type)
 		{
 			case "1":  // GIF
 				$this->rImage = imagecreatefromgif($_inFile);
 				break;
-				
+
 			case "2":  // JPG
 				$this->rImage = imagecreatefromjpeg($_inFile);
 				break;
-				
+
 			case "3":  // PNG
 				$this->rImage = imagecreatefrompng($_inFile);
+				// imagecolorallocatealpha($this->rImage, 255, 255, 255, 127);
 				break;
-				
+
 			case "15": // WBMP
 				$this->rImage = imagecreatefromwbmp($_inFile);
 				break;
-				
+
 			case "16": // XBM
 				$this->rImage = imagecreatefromxbm($_inFile);
 				break;
-				
+
 			case "4":  // SWF
 			case "5":  // PSD
 			case "6":  // BMP
@@ -65,29 +67,29 @@ class Image
 				return false;
 				break;
     	}
-		
+
 		return true;
 	}
-	
+
 	/**
 		Returns image resource
 		@return (resource) - image resource
 	**/
-	public function getImage() 
+	public function getImage()
 	{
 		return $this->rImage;
 	}
-	
+
 	/**
 		Sets image resource.
 		@param (resource) $rImage - image resource
 	**/
-	public function setImage($rImage) 
+	public function setImage($rImage)
 	{
 		imagedestroy($this->rImage);
 		$this->rImage = $rImage;
 	}
-	
+
 	/**
 		Returns size X of image
 		@return (int) - size X of image
@@ -96,70 +98,70 @@ class Image
 	{
 		return imagesx($this->getImage());
 	}
-	
+
 	/**
 		Sets size X of image. Image will be resized to $iNewSizeX by width
 		@param (int) $iNewSizeX - new size X
 	**/
-	public function setSizeX($iNewSizeX) 
+	public function setSizeX($iNewSizeX)
 	{
 		$this->resize($iNewSizeX, imagesy($this->getImage()));
 	}
-	
+
 	/**
 		Returns size Y of image
 		@return (int) - size Y of image
 	**/
-	public function getSizeY() 
+	public function getSizeY()
 	{
 		return imagesy($this->getImage());
 	}
-	
+
 	/**
 		Sets size Y of image. Image will be resized to $iNewSizeX by height
 		@param (int) $iNewSizeY - new size Y
 	**/
-	public function setSizeY($iNewSizeY) 
+	public function setSizeY($iNewSizeY)
 	{
 		$this->resize(imagesx($this->getImage()), $iNewSizeY);
 	}
-	
+
 	/**
-		Performs an image resizing constrain proportions. If new sizes do not correspond to image actial sizes, 
-		image is filled with given background color. 
+		Performs an image resizing constrain proportions. If new sizes do not correspond to image actial sizes,
+		image is filled with given background color.
 		@param (int) $iNewSizeX - new size X
 		@param (int) $iNewSizeY - new size Y
 		@param (string) $sHexBkgColor - hexadecimal string of RGB-color representation for background color
 	**/
-	public function resize($iNewSizeX = 0, $iNewSizeY = 0, $sHexBkgColor = "FFFFFF") 
+	public function resize($iNewSizeX = 0, $iNewSizeY = 0, $sHexBkgColor = "FFFFFF")
 	{
 		$rImage = $this->getImage();
 		$iSourceX = $this->getSizeX();
 		$iSourceY = $this->getSizeY();
-        
+
 		$iW = $iNewSizeX;
 		$iH = $iNewSizeY;
-        
-		
+
+
 		//$iNewSizeX = min($iNewSizeX, $iSourceX); - reduce only
 		//$iNewSizeY = min($iNewSizeY, $iSourceY);
-        
+
 		$fAspectX = $iNewSizeX / $iSourceX;
 		$fAspectY = $iNewSizeY / $iSourceY;
-		
-		if (!$iH) 
+
+		if (!$iH)
 		{
 			$fAspect = $fAspectX;
 			$iSourceY = imagesy($rImage);
 			$iNewSizeY = $iSourceY * $fAspect;
-		} 
+		}
 		else {
 			$fAspect = min($fAspectX, $fAspectY);
 			$iNewSizeX = $iSourceX * $fAspect;
 		}
 		$iDestX = $iSourceX * $fAspect;
 		$iDestY = $iSourceY * $fAspect;
-		
+
 		$rImage = imagecreatetruecolor($iNewSizeX, $iNewSizeY);
 		$aColor = $this->getAColor($sHexBkgColor);
 		imagefill($rImage, 1, 1, imagecolorallocate($rImage, $aColor[0], $aColor[1], $aColor[2]));
@@ -167,80 +169,80 @@ class Image
 		// imagecopyresized($rImage, $this->getImage(), ($iNewSizeX - $iDestX) / 2, ($iNewSizeY - $iDestY) / 2, 0, 0, $iDestX, $iDestY, $iSourceX, $iSourceY);
 		$this->setImage($rImage);
 	}
-	
-	/** 
-		Divides hexadecimal string of RGB-color representation into a RGB-color array accordingly. 
+
+	/**
+		Divides hexadecimal string of RGB-color representation into a RGB-color array accordingly.
 		If $i parameter is passed, returns only appropriate color component (R,G or B)
-		@param (string) $sHexColor - hexadecimal string of RGB-color representation. 
+		@param (string) $sHexColor - hexadecimal string of RGB-color representation.
 		@param (int) $i - index of RGB-color array. Can take values from 0 to 2 or false.
 		@return (int/array) - color component (R,G or B) if $i is passed or RGB-array if $i is ommited
 	**/
 	public function getAColor($sHexColor, $i = false)
 	{
 		$_return = array(hexdec(substr($sHexColor, 0, 2)), hexdec(substr($sHexColor, 2, 2)), hexdec(substr($sHexColor, 4, 2)));
-		if ($i !== false) 
+		if ($i !== false)
 			$_return = $_return[$i];
 		return $_return;
 	}
-	
-	/** 
+
+	/**
 		Allocates and returns integer color representation for hexadecimal string of RGB-color representation
 		@param (string) $sHexColor - hexadecimal string of RGB-color representation
 		@return (int) - integer color representation
 	**/
-	public function getColor($sHexColor) 
+	public function getColor($sHexColor)
 	{
 		$aColor = $this->getAColor($sHexColor);
 		return imagecolorallocate(&$this->rImage, $aColor[0], $aColor[1], $aColor[2]);
 	}
-	
+
 	/**
 		Outputs image as GIF. If $sOutFile is passed, image is saved as GIF file else performs raw output of GIF image to browser
 		@param (string) $sOutFile - file name for GIF image. If ommited, performs raw output to browser
 		@param (resource) $rImage - image resource to create GIF from. By default it is inner image.
 	**/
-	public function outputGif($sOutFile = false, $rImage = false) 
+	public function outputGif($sOutFile = false, $rImage = false)
 	{
 		if (!$rImage) {
 			$rImage = $this->getImage();
 		}
-		if ($sOutFile === false) 
+		if ($sOutFile === false)
 		{
 			header("Content-type: image/gif");
 			imagegif($rImage);
-		} 
-		else 
+		}
+		else
 			imagegif($rImage, $sOutFile);
 	}
-	
-	public function outputJpg($sOutFile = false, $rImage = false) 
+
+	public function outputJpg($sOutFile = false, $rImage = false)
 	{
 		if (!$rImage) {
 			$rImage = $this->getImage();
 		}
-		if ($sOutFile === false) 
+		if ($sOutFile === false)
 		{
 			header("Content-type: image/jpeg");
 			imagejpeg($rImage);
-		} 
-		else 
+		}
+		else
 			imagejpeg($rImage, $sOutFile, 90);
 	}
-	
-	public function outputPng($sOutFile = false, $rImage = false) 
+
+	public function outputPng($sOutFile = false, $rImage = false)
 	{
 		if (!$rImage) {
 			$rImage = $this->getImage();
 		}
-		if ($sOutFile === false) 
+		if ($sOutFile === false)
 		{
 			header("Content-type: image/png");
 			imagepng($rImage);
-		} 
-		else 
+		}
+		else
 			imagepng($rImage, $sOutFile);
 	}
-	
+
 	/**
 		Draws a line of given thickness.
 		@param (int) $iX1 - X1 coordinate of line
@@ -250,22 +252,22 @@ class Image
 		@param (string) $sHexColor - hexadecimal string of RGB-color representation
 		@param (int) $iThick - thickness of line in pixels
 	**/
-	public function linethick($iX1, $iY1, $iX2, $iY2, $sHexColor, $iThick = 1) 
+	public function linethick($iX1, $iY1, $iX2, $iY2, $sHexColor, $iThick = 1)
 	{
 		$rImage = $this->getImage();
 		$iColor = $this->getColor($sHexColor);
-		
+
 	    /* this way it works well only for orthogonal lines
 	    imagesetthickness($rImage, $iThick);
 	    return imageline($rImage, $iX1, $iY1, $iX2, $iY2, $iColor);
 	    */
-	    if ($iThick == 1) 
+	    if ($iThick == 1)
 	        return imageline($rImage, $iX1, $iY1, $iX2, $iY2, $iColor);
-		
+
 	    $t = $iThick / 2 - 0.5;
-	    if ($iX1 == $iX2 || $iY1 == $iY2) 
+	    if ($iX1 == $iX2 || $iY1 == $iY2)
 	        return imagefilledrectangle($rImage, round(min($iX1, $iX2) - $t), round(min($iY1, $iY2) - $t), round(max($iX1, $iX2) + $t), round(max($iY1, $iY2) + $t), $iColor);
-		
+
 	    $k = ($iY2 - $iY1) / ($iX2 - $iX1); //y = kx + q
 	    $a = $t / sqrt(1 + pow($k, 2));
 	    $aPoints = array(
@@ -273,45 +275,45 @@ class Image
 	        round($iX1 - (1-$k)*$a), round($iY1 - (1 + $k) * $a),
 	        round($iX2 + (1+$k)*$a), round($iY2 - (1 - $k) * $a),
 	        round($iX2 + (1-$k)*$a), round($iY2 + (1 + $k) * $a),
-	    );    
+	    );
 	    imagefilledpolygon($rImage, $aPoints, 4, $iColor);
 	    imagepolygon($rImage, $aPoints, 4, $iColor);
 	}
-	
+
 	/**
-		Draws a rectangle. $xHexColor is a color parameter to fill a rectangle. If a string is passed, 
-		this parameter is considered as hexadecimal string of RGB-color representation. If an array is passed, 
+		Draws a rectangle. $xHexColor is a color parameter to fill a rectangle. If a string is passed,
+		this parameter is considered as hexadecimal string of RGB-color representation. If an array is passed,
 		then this parameter must contain two elements setting a begin color of gradient and end color of gradient.
 		By now only vertical gradient is implemented.
 		@param (int) $iX1 - X1 coordinate of rectangle
 		@param (int) $iY1 - Y1 coordinate of rectangle
 		@param (int) $iX2 - X2 coordinate of rectangle
 		@param (int) $iY2 - Y2 coordinate of rectangle
-		@param (string/array) $xHexColor - hexadecimal string of RGB-color or an array with gradient colors to fill a rectangle. 
+		@param (string/array) $xHexColor - hexadecimal string of RGB-color or an array with gradient colors to fill a rectangle.
 	**/
-	public function filledRectangle($iX1, $iY1, $iX2, $iY2, $xHexColor) 
+	public function filledRectangle($iX1, $iY1, $iX2, $iY2, $xHexColor)
 	{
-		if (is_array($xHexColor)) 
+		if (is_array($xHexColor))
 		{
 			// gradient passed
 			$aGradient = $xHexColor;
 			$aColor1 = $this->getAColor($aGradient[0]);
 			$aColor2 = $this->getAColor($aGradient[1]);
-			
+
 			// gradient type - vertical
 			$aOffsetColor = array($aColor2[0] - $aColor1[0], $aColor2[1] - $aColor1[1], $aColor2[2] - $aColor1[2]);
-			for ($iY = $iY1; $iY <= $iY2; $iY++) 
+			for ($iY = $iY1; $iY <= $iY2; $iY++)
 			{
 				$offset = ($iY - $iY1) / ($iY2 - $iY1);
-				$iColor = imagecolorallocate($this->getImage(), 
-					round($aColor1[0] + $aOffsetColor[0] * $offset), 
+				$iColor = imagecolorallocate($this->getImage(),
+					round($aColor1[0] + $aOffsetColor[0] * $offset),
 					round($aColor1[1] + $aOffsetColor[1] * $offset),
 					round($aColor1[2] + $aOffsetColor[2] * $offset)
 				);
 				imageline($this->getImage(), $iX1, $iY, $iX2, $iY, $iColor);
 			}
-		} 
-		else 
+		}
+		else
 			imagefilledrectangle($this->getImage(), $iX1, $iY1, $iX2, $iY2, $this->getColor($xHexColor));
 	}
 }
